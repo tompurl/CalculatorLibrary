fa7de6e31bf221f25283ed95a9067ec45157ad63
Tom Purl <tom.purl@wi-tronix.com>
2022-06-21 17:00:53 -0500

    Initial commit

---
+++ shell.nix
@@ -0,0 +1,12 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShell {
+  buildInputs = [
+    pkgs.python39Full
+    pkgs.poetry
+    pkgs.nodePackages.pyright
+
+    # keep this line if you use bash
+    pkgs.bashInteractive
+  ];
+}
